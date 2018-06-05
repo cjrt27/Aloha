1f92390e013e949f4cffdaafc52edc0738cdd0b6
cjrt27 <cameron.jr.thorn@gmail.com>
2018-06-05 14:47:28 +0100

    Rework on project: using 'prettier' on files and making slight modifications to README and email submission in JS doc

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a project which tests students abilities relating to HTML, CSS and Javas
 
 This webpage is fully compatable across all browsers and devices.
 
-Some challanges including learning css flexbox and getting the smooth scroll to work correctly in jQuery. 
+Some challanges including learning css flexbox and getting the smooth scroll to work correctly in jQuery. At the completion of this project, I can say that I learned a great deal about HTML, CSS and started to get comfertable with simple Javascript functions.
 
 # Technologies Used
 * Git Bash (https://git-for-windows.github.io/)
--- index.js
+++ index.js
@@ -1,114 +1,104 @@
 $(function() {
+  //*-----------------------Smooth Scrolling Start------------------------------------------*//
 
-//*---------------------------------------------------------------------Smooth Scrolling Start------------------------------------------*//
+  $("a").click(function(e) {
+    e.preventDefault();
+    $("html").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top
+      },
+      500
+    );
+  });
 
-$('a').click( function(e) {
-     e.preventDefault()
-	 $('html').animate({
-          scrollTop: $($(this).attr('href')).offset().top
-	 },500);
+  //*-------------------------Smooth Scrolling End------------------------------------------*//
+
+  //*--------------------------Cart Counter Start------------------------------------------*//
+  var itemCount = 0;
+
+  $(".addToCart").click(function() {
+    itemCount++;
+    $("#itemCount")
+      .html(itemCount)
+      .css("display", "block");
+  });
+
+  //*------------------------------Cart Counter End------------------------------------------*//
+
+  //*-------------------------------Carosel Start------------------------------------------*//
+
+  $(document).ready(function() {
+    $(".mostLovedCarosel").slick({
+      autoplay: true,
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1239,
+          settings: {
+            arrows: false,
+            autoplay: true,
+            dots: true,
+            slidesToShow: 4,
+            slidesToScroll: 4
+          }
+        },
+        {
+          breakpoint: 1025,
+          settings: {
+            arrows: false,
+            autoplay: true,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+
+        {
+          breakpoint: 599,
+          settings: {
+            arrows: false,
+            autoplay: true,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+
+  //*-----------------------------------Carosel End------------------------------------------*//
+
+  //*----------------------Email Input Alert Message Start------------------------------------------*//
+
+  $(document).ready(function() {
+    $("#myform").submit(function(e) {
+      var sEmail = $("#emailinput").val();
+      // Checking Empty Fields
+      if ($.trim(sEmail).length == 0) {
+        alert("Field is Mandatory");
+        e.preventDefault();
+      }
+      if (validateEmail(sEmail)) {
+		  e.preventDefault();
+        alert("Thanks for Subscribing!");
+      } else {
+        alert("Please Enter a Valid Email Address");
+        e.preventDefault();
+      }
+    });
+  });
+  // Function that validates email address through a regular expression.
+  function validateEmail(sEmail) {
+    var filter = /^[\w-.+]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+    if (filter.test(sEmail)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  //*---------------------------------------------------------------------Email Input Alert Message End------------------------------------------*//
 });
-
-
-
-
-
-//*---------------------------------------------------------------------Smooth Scrolling End------------------------------------------*//
-
-//*---------------------------------------------------------------------Cart Counter Start------------------------------------------*//
-var itemCount = 0;
-
-$('.addToCart').click(function (){
-  itemCount ++;
-  $('#itemCount').html(itemCount).css('display', 'block');
-}); 
-
-//*---------------------------------------------------------------------Cart Counter End------------------------------------------*//
-
-//*---------------------------------------------------------------------Carosel Start------------------------------------------*//
-
-
-$(document).ready(function(){
-  $('.mostLovedCarosel').slick({
-		autoplay: true,
-		dots:true,
-		slidesToShow: 4,
-		slidesToScroll: 4,
-		arrows: false,
-		responsive: 
-		[{
-			breakpoint: 1239,
-				settings: 
-					{
-					arrows: false,
-					autoplay: true,
-					dots:true,
-					slidesToShow: 4,
-					slidesToScroll: 4}
-		},
-		{
-			breakpoint: 1025,
-				settings: 
-					{
-					arrows: false,
-					autoplay: true,
-					dots:true,
-					slidesToShow: 2,
-					slidesToScroll: 2}
-		},
-		
-			{
-				breakpoint: 599,
-				settings: 
-					{
-					arrows: false,
-					autoplay: true,
-					dots:true,
-					slidesToShow: 1,
-					slidesToScroll: 1
-					}
-			}
-		]
-							});
-})
-
-//*---------------------------------------------------------------------Carosel End------------------------------------------*//
-
-
-//*---------------------------------------------------------------------Email Input Alert Message Start------------------------------------------*//
-
-
-$(document).ready(function(e) {
-$('#subscribebutton').click(function() {
-var sEmail = $('#emailinput').val();
-// Checking Empty Fields
-if ($.trim(sEmail).length == 0) {
-alert('Field is Mandatory');
-e.preventDefault();
-}
-if (validateEmail(sEmail)) {
-alert('Thanks for Subscribing!');
-}
-else {
-alert('Please Enter a Valid Email Address');
-e.preventDefault();
-}
-});
-});
-// Function that validates email address through a regular expression.
-function validateEmail(sEmail) {
-var filter = /^[\w-.+]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
-if (filter.test(sEmail)) {
-return true;
-}
-else {
-return false;
-}
-}
-//*---------------------------------------------------------------------Email Input Alert Message End------------------------------------------*//
-
-
-
-})
-
-
